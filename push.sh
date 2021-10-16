20e8e6ff8740ecf505fd6fb5eb926de18de3c860
Rafael Lefever <rafael.lefever@discomco.pl>
2021-10-16 20:13:59 +0200

    v1.10.1

--- push.sh
+++ push.sh
@@ -1,27 +1,8 @@
 #! /bin/bash
 clear
-echo
-echo '------------------------------------'
-echo pushing version $1 to develop branch
-echo '------------------------------------'
-echo
-git checkout develop
-git push --set-upstream origin develop
-
-echo  
-echo
-echo 'cooling down for 30 seconds..'
-sleep 30s
-echo
-echo
-clear
 echo '-----------------------------------'
 echo pushing version "$1" to main branch
 echo '-----------------------------------'
-git checkout main
 git add .
 git commit -m "$1" -a
-
-git merge develop
 git push
-git checkout develop
