bf77b04b2e39784e833798f46bdb6d274f3c002e
Rafael Lefever <rafael.lefever@discomco.pl>
2021-10-16 18:57:24 +0200

    v1.10.1

--- cid/build.sh
+++ cid/build.sh
@@ -3,9 +3,9 @@ dotnet nuget locals --clear all
 dotnet restore --disable-parallel
 echo  "branch=$CI_COMMIT_REF_NAME"
 if [ "$CI_COMMIT_REF_NAME" = "master" ]; then
-  dotnet build -c Release --ignore-failed-sources M5x.root.sln 
+  dotnet build -o ./BLD -c Release --ignore-failed-sources M5x.root.sln 
 else
-  dotnet build -c  Debug --version-suffix "debug" M5x.root.sln 
+  dotnet build -o ./BLD -c  Debug --version-suffix "debug" M5x.root.sln 
 fi
 
 
